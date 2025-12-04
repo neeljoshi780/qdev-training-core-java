5d75519fae1710db7fe4ef8cb82750d75727732f
Neel Joshi <neeljoshi780@gmail.com>
2025-12-04 10:54:08 +0530

    Add HelloWorld.java for Practical 1

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+ * Simple program to print "Hello World" to the console.
+ * This program demonstrates the use of basic Java syntax, including class definition,
+ * the `main` method, and printing to the console using `System.out.println`.
+ * 
+ * @author Neel Joshi
+ * @version 1.0.0
+ * @since 2025-12-02
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        
+        // Print "Hello World!" to the console
+        System.out.println("Hello World!");
+    }
+}
